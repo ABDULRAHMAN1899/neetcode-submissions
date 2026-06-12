6c2ac7b5e225ba27a5f78e2874b59e3fe3833300
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-06-12 16:46:48 +0530

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.cpp
@@ -0,0 +1,75 @@
+class MinStack {
+private:
+    // Initialize a stack
+    stack <long long> st;
+    // To store the minimum value 
+    long long mini;
+    
+public:
+    
+    // Empty Constructor
+    MinStack() {
+    }
+    
+    // Method to push a value in stack
+    void push(int value) {
+        
+        // If stack is empty
+        if(st.empty()) {
+            //Update the minimum value
+            mini = value;
+            
+            // Push current value as minimum
+            st.push( value );
+            return;
+        }
+        
+        // If the value is greater than the minimum
+        if(value > mini) {
+            st.push(value);
+        }
+        else {
+            // Add the modified value to stack
+            st.push(2LL * value - mini);
+            // Update the minimum
+            mini = value;
+        }
+    }
+    
+    // Method to pop a value from stack
+    void pop() {
+        // Base case
+        if(st.empty()) return;
+        
+        // Get the top
+        long long x = st.top();
+        st.pop(); // Pop operation
+        
+        // If the modified value was added to stack
+        if(x < mini) {
+            // Update the minimum
+            mini = 2 * mini - x;
+        }
+    }
+    
+    // Method to get the top of stack
+    int top() {
+        // Base case
+        if(st.empty()) return -1;
+        
+        // Get the top
+        long long x = st.top();
+        
+        // Returnn top if minimum is less than the top
+        if(mini < x) return x;
+        
+        //Otherwise return mini
+        return mini;
+    }
+    
+    // Method to get the minimum in stack
+    int getMin() {
+        // Return the minimum
+        return mini;
+    }
+};
