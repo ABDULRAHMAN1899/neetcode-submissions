e93ab72ad0bd34d543e8f33aa4707abc86b68d7c
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-07-01 15:50:40 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
+        }
+        while(!(pq.size()<=1)){
+            int l=pq.top();
+            pq.pop();
+            int sl=pq.top();
+            if(l==sl){
+                pq.pop();
+            }
+            else {
+                pq.pop();
+                pq.push(l-sl);
+            }
+        }
+        if(pq.size()==0){
+            return 0;
+        }
+        return pq.top();
+    }
+};
