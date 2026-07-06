a0611704e9b404b10f151878ddb474130138a80d
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-07-06 18:16:46 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans,vector<bool>& used){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(used[i]){
+                continue;
+            }
+            used[i]=true;
+            ds.push_back(nums[i]);
+            backtrack(nums,ds,ans,used);
+            ds.pop_back();
+            used[i]=false;
+        }
+        
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<bool> used(nums.size(),false);
+        backtrack(nums,ds,ans,used);
+        return ans;
+    }
+};
