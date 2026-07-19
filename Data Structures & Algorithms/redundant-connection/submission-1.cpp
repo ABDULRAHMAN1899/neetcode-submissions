c5bea31e8399d44986b15ca8f7bc1a7892c45617
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-07-19 17:58:29 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,50 @@
+class dsu{
+    vector<int> rank,parent;
+public:
+    dsu(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int findupar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findupar(parent[node]);
+    }
+    bool unionbyrank(int u,int v){
+        int ulp_u=findupar(u);
+        int ulp_v=findupar(v);
+        if(ulp_u==ulp_v){
+            return false;
+        }
+        else if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_u]=ulp_v;
+            rank[ulp_v]++;
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        dsu ds(n);
+        for(auto it: edges){
+            int u=it[0];
+            int v=it[1];
+            if(!ds.unionbyrank(u,v)){
+                return {u,v};
+            }
+        }
+        return {};
+    }
+};
