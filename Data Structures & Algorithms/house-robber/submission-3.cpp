53bb0853fb9fc7b72413e18c55103bea8d4a5921
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-07-27 16:24:20 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        int prev2=0;
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+            int rob=nums[i]+prev2;
+            int drob=prev;
+            int curri=max(rob,drob);
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+        
+    }
+};
