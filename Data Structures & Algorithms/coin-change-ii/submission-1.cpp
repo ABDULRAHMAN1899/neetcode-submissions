88d0b2b4d5cd65187f2504ffabfa75833201a293
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-08-05 19:09:56 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int ind,int target,vector<int>& coins,vector<vector<int>>& dp){
+        if(ind==0){
+            if(target==0){
+                return 1;
+            }
+            else if(target%coins[ind]==0){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[ind][target]!=-1){
+            return dp[ind][target];
+        }
+        int pick=0;
+        if(target>=coins[ind]){
+            pick=solve(ind,target-coins[ind],coins,dp);
+        }
+        int notpick=solve(ind-1,target,coins,dp);
+        return dp[ind][target]=pick+notpick;
+
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>> dp (n,vector<int> (amount+1,-1));
+        return solve(n-1,amount,coins,dp);
+    }
+};
