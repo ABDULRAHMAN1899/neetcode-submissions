5a110376ce2111ba9bad7eb6abca7ab88ebfb5b0
ABDULRAHMAN1899 <abdulmd8686@gmail.com>
2026-08-22 22:18:28 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int n=s.length();
+        vector<vector<string>> res;
+        vector<string> path;
+        func(0,s,path,res);
+        return res;
+        
+    }
+    void func(int ind,string& s,vector<string>& path,vector<vector<string>>& res){
+        if(ind==s.length()){
+            res.push_back(path);
+            return;
+        }
+        for(int i=ind;i<s.length();i++){
+            if(ispalindrome(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                func(i+1,s,path,res);
+                path.pop_back();
+            }
+        }
+    }
+    bool ispalindrome(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
